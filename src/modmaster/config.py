c5b08bb26659bdabbb7ff78440e83fbe2fbb91a8
CPearl0 <1269596929@qq.com>
2025-07-13 14:20:21 +0800

    Global config

--- src/modmaster/config.py
+++ src/modmaster/config.py
@@ -1,12 +1,25 @@
 import configparser
 from pathlib import Path
 from typing import Dict
+import os
 
 
 class config_manager:
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super().__new__(cls)
+            cls._instance._initialized = False
+        return cls._instance
+
     def __init__(self) -> None:
+        if self._initialized:
+            return
         self.config_path = Path("configs/config.ini")
         self.config = configparser.ConfigParser()
+        self.load_config()
+        self._initialized = True
 
     def default_config(self) -> Dict[str, Dict[str, str]]:
         return {
@@ -40,11 +53,28 @@ class config_manager:
     def load_config(self) -> configparser.ConfigParser:
         if not self.config_exists():
             self.create_default_config()
-            return self.config
-
+            print("已生成配置文件，请在config/config.ini中进行配置")
+            exit()
         try:
             self.config.read(self.config_path)
         except configparser.Error as e:
             print(f"⚠️ 加载配置文件时出错: {e}")
             self.create_default_config()
+
+        # Set the environment
+        api_key = self.get("zhipu", "api-key")
+        os.environ["ZHIPUAI_API_KEY"] = api_key
+
+        langsmith_api_key = self.get("langchain", "api-key")
+        os.environ["LANGSMITH_TRACING"] = "true"
+        os.environ["LANGSMITH_END_POINT"] = "https://api.smith.langchain.com"
+        os.environ["LANGSMITH_API_KEY"] = langsmith_api_key
+        os.environ["LANGSMITH_PROJECT"] = "modmaster"
+
         return self.config
+
+    def get(self, section: str, option: str) -> str:
+        return self.config.get(section, option)
+
+
+config = config_manager()
